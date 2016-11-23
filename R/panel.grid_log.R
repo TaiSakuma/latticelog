d6f0f942d2846e9f658ea4f9cd4e872a5b82a554
Tai Sakuma <tai.sakuma@gmail.com>
2016-11-23 14:39:56 +0100

    add panel.grid_logx()

--- R/panel.grid_log.R
+++ R/panel.grid_log.R
@@ -1,9 +1,23 @@
 
 ##____________________________________________________________________________||
-log10.y.grid.at <- function(y.at)
+log10.grid.at <- function(at)
   {
-    y <- sort(as.vector(sapply(1:9*10, function(x) log10(x*10^{(min(y.at) - 1):(max(y.at) - 1)}))))
-    y[y <= max(y.at)]
+    ret <- sort(as.vector(sapply(1:9*10, function(x) log10(x*10^{(min(at) - 1):(max(at) - 1)}))))
+    ret[ret <= max(at)]
+  }
+
+##____________________________________________________________________________||
+#' @export
+panel.grid_logx <- function(col = reference.line$col, lty = reference.line$lty,
+                           lwd = reference.line$lwd, lwd.minor = reference.line$lwd*0.5, ...)
+
+  {
+    reference.line <- trellis.par.get("reference.line")
+    lim <- current.panel.limits()
+    x.at <- c(floor(lim$xlim[1]), ceiling(lim$xlim[2]))
+    x.grid.at <- log10.grid.at(x.at)
+    sapply(x.grid.at, function(x) panel.lines(c(x, x), c(lim$ylim[1], lim$ylim[2]), col = col, lwd = lwd.minor))
+    sapply(x.at, function(x) panel.lines(c(x, x), c(lim$ylim[1], lim$ylim[2]), col = col, lwd = lwd))
   }
 
 ##____________________________________________________________________________||
@@ -15,7 +29,7 @@ panel.grid_logy <- function(col = reference.line$col, lty = reference.line$lty,
     reference.line <- trellis.par.get("reference.line")
     lim <- current.panel.limits()
     y.at <- c(floor(lim$ylim[1]), ceiling(lim$ylim[2]))
-    y.grid.at <- log10.y.grid.at(y.at)
+    y.grid.at <- log10.grid.at(y.at)
     sapply(y.grid.at, function(x) panel.lines(c(lim$xlim[1], lim$xlim[2]), c(x, x), col = col, lwd = lwd.minor))
     sapply(y.at, function(x) panel.lines(c(lim$xlim[1], lim$xlim[2]), c(x, x), col = col, lwd = lwd))
   }
--- visual_test/test_panel.grid_log.R
+++ visual_test/test_panel.grid_log.R
@@ -1,7 +1,7 @@
 library(lattice)
 
 ##____________________________________________________________________________||
-vcontext("panel.grid_logy")
+vcontext("panel.grid_log")
 
 ##____________________________________________________________________________||
 x <- 0:5
@@ -9,7 +9,23 @@ y <- c(4, -3, 2, 4, 1, 0)
 p <- xyplot(y ~ x, panel = function() { panel.grid_logy() })
 print(p)
 set_last_plot(p)
-save_vtest("panel.grid_logy 1")
+save_vtest("panel.grid_log 1")
+
+##____________________________________________________________________________||
+x <- 0:5
+y <- c(4, -3, 2, 4, 1, 0)
+p <- xyplot(y ~ x, panel = function() { panel.grid_logx() })
+print(p)
+set_last_plot(p)
+save_vtest("panel.grid_log 2")
+
+##____________________________________________________________________________||
+x <- 0:5
+y <- c(4, -3, 2, 4, 1, 0)
+p <- xyplot(y ~ x, panel = function() { panel.grid_logx(); panel.grid_logy() })
+print(p)
+set_last_plot(p)
+save_vtest("panel.grid_log 3")
 
 ##____________________________________________________________________________||
 end_vcontext()
